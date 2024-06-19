4591aab1487ceec1708ac44c6a69e476212d31e7
mohdumair8896 <sajidali27726@gmail.com>
2024-06-19 16:29:53 +0530

    Script for Installing any software

---
+++ InstallSoftware.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "script to install git "
+echo "Installation started"
+if [ "$(uname)" == "Linux" ];
+then
+        echo "this is linux,installing git"
+        yum install git -y
+elif [ "$(uname)" == "Darwin" ];
+then
+        echo "this is not linux"
+        echo "this is Macos"
+        brew install git
+else
+        echo "not installing"
+fi
